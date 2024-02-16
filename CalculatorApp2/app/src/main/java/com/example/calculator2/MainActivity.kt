fad6e942f9ec8429a9e7dd7dae88cc4c77ac1f9f
junior123w <113151392+junior123w@users.noreply.github.com>
2024-02-15 20:03:37 -0500

    created files for assignment 2

---
+++ CalculatorApp2/app/src/main/java/com/example/calculator2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.calculator2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Name: Naimil Shah
+ * Student Number:200522618
+ * App Name:Calculator
+ * Date: 1st Feb 2024
+ * Version: v 01.6*/
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
