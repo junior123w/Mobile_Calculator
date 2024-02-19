265b0abb94d7d615eff3b69fba1f78dd8428199f
junior123w <113151392+junior123w@users.noreply.github.com>
2024-02-18 23:32:17 -0500

    final code

--- CalculatorApp2/app/src/main/java/com/example/calculator2/MainActivity.kt
+++ CalculatorApp2/app/src/main/java/com/example/calculator2/MainActivity.kt
@@ -9,7 +9,10 @@ import com.example.calculator2.databinding.ActivityMainBinding
  * Student Number:200522618
  * App Name:Calculator2
  * Date: 18st Feb 2024
- * Version: v 02.8*/
+ * Version: v 02.8
+ * Description: This is my calculator application it does the order of operation function still in early stages cannot
+ do the BEDMAS functionality without external libraries*/
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var calculator:Calculator
